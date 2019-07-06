54fe2250759fc822e57726f28e26639ad3627e0e
Varunram <vrg2009@ymail.com>
2019-07-06 17:01:18 -0400

    make blind schnorr sigs work

--- crypto/btc/statechain/statechain.go
+++ crypto/btc/statechain/statechain.go
@@ -39,7 +39,7 @@ func BytesToNum(byteString []byte) *big.Int {
 	return new(big.Int).SetBytes(byteString)
 }
 
-func SchnorrGetK() []byte {
+func GetRandomness() []byte {
 	k := make([]byte, 32)
 	_, err := io.ReadFull(rand.Reader, k)
 	if err != nil {
@@ -78,13 +78,69 @@ func SchnorrVerify(sig []byte, Rx, Ry *big.Int, Px, Py *big.Int, m string) bool 
 	ePx, ePy := Curve.ScalarMult(Px, Py, eByte) // H(R,P,m) * P
 
 	rightX, rightY := Curve.Add(Rx, Ry, ePx, ePy)
-
+	log.Println("VerifyServer=", rightX, rightY)
 	sx, sy := Curve.ScalarBaseMult(sig) // s*G
-
+	log.Println("SIGX, SIGY", sx, sy)
 	if sx.Cmp(rightX) == 0 && sy.Cmp(rightY) == 0 {
 		return true
 	}
 	return false
+}
+
+func BlindServerNonce() ([]byte, *big.Int, *big.Int) {
+	k := make([]byte, 32)
+	_, err := io.ReadFull(rand.Reader, k)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Rx, Ry := Curve.ScalarBaseMult(k) // R = k*G
+	return k, Rx, Ry
+}
+
+func BlindClientBlind(Rx *big.Int, Ry *big.Int, m string, Px, Py *big.Int, privkey *big.Int) (
+	[]byte, []byte, *big.Int, *big.Int, []byte, []byte) {
+	alpha := GetRandomness()
+	beta := GetRandomness()
+
+	alphaGX, alphaGY := Curve.ScalarBaseMult(alpha)
+	betaPX, betaPY := Curve.ScalarMult(Px, Py, beta)
+
+	// need to add Rx, alphax, betapx
+	tempX, tempY := Curve.Add(Rx, Ry, alphaGX, alphaGY) // R + alpha*G
+	RprX, RprY := Curve.Add(tempX, tempY, betaPX, betaPY) // R + alpha*G + beta*P
+
+	Rpr := append(RprX.Bytes(), RprY.Bytes()...)
+	P := append(Px.Bytes(), Py.Bytes()...)
+
+	cpr := btcutils.Sha256(append(append(Rpr, P...), []byte(m)...))
+
+	ePx, ePy := Curve.ScalarMult(Px, Py, cpr) // H(R,P,m) * P
+	rightX, rightY := Curve.Add(RprX, RprY, ePx, ePy)
+	log.Println("CLIENTVERIFY=", rightX, rightY)
+
+	c := new(big.Int).Add(BytesToNum(cpr), BytesToNum(beta))
+
+	return alpha, beta, RprX, RprY, cpr, c.Bytes()
+}
+
+func BlindServerSign(kByte []byte, cByte []byte, privkey *big.Int) []byte {
+	k := BytesToNum(kByte)
+	c := BytesToNum(cByte)
+
+	cx := new(big.Int).Mul(c, privkey)
+	ePx, ePy := Curve.ScalarBaseMult(cx.Bytes())
+	log.Println("cP server ", ePx, ePy)
+
+	sig := new(big.Int).Add(k, cx)
+	return sig.Bytes()
+}
+
+func BlindClientUnblind(alphaByte []byte, sigByte []byte) []byte {
+	alpha := BytesToNum(alphaByte)
+	s := BytesToNum(sigByte)
+	spr := new(big.Int).Add(s, alpha)
+	return spr.Bytes()
 }
 
 // SerializeCompressed serializes a public key in a 33-byte compressed format.
@@ -118,12 +174,12 @@ func requestNewPubkey(userPubkey string) ([]byte, error) {
 	var serverPubkey []byte
 	var err error
 
-	_, serverPubkeyX, serverPubkeyY, err := GetNewKeys()
+	_, serverPx, serverPy, err := GetNewKeys()
 	if err != nil {
 		return nil, errors.Wrap(err, "could not generate new pubkey, quitting")
 	}
 
-	Storage[userPubkey] = SerializeCompressed(serverPubkeyX, serverPubkeyY)
+	Storage[userPubkey] = SerializeCompressed(serverPx, serverPy)
 	return serverPubkey, nil
 }
 
@@ -163,19 +219,44 @@ func broadcastTx(tx string) error {
 	return nil
 }
 
-func main() {
-	privkey, pubkeyX, pubkeyY, err := GetNewKeys()
+func testSchnorr() {
+	privkey, Px, Py, err := GetNewKeys()
 	if err != nil {
 		log.Fatal(err)
 	}
 	//log.Println("PRIVKEY: ", privkey, "PUBKEY: ", pubkey, len(privkey), len(pubkey))
-	k := SchnorrGetK()
-	sig, Rx, Ry := SchnorrSign(k, pubkeyX, pubkeyY, "hello world", BytesToNum(privkey))
-	log.Println("SCHNORR SIG: ", sig)
+	k := GetRandomness()
+	sig, Rx, Ry := SchnorrSign(k, Px, Py, "hello world", BytesToNum(privkey))
+	// log.Println("SCHNORR SIG: ", sig)
 
-	if !SchnorrVerify(sig, Rx, Ry, pubkeyX, pubkeyY, "hello world") {
+	if !SchnorrVerify(sig, Rx, Ry, Px, Py, "hello world") {
 		log.Fatal(errors.New("schnorr sigs don't match"))
 	} else {
 		log.Println("Schnorr signatures work")
 	}
+}
+
+func main() {
+	privkey, Px, Py, err := GetNewKeys()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Println("PRIVKEY: ", privkey, "PUBKEY: ", pubkey, len(privkey), len(pubkey))
+	k, Rx, Ry := BlindServerNonce()
+
+	alpha, _, RprX, RprY, _, c := BlindClientBlind(Rx, Ry, "hello world", Px, Py, BytesToNum(privkey))
+	//log.Println("ALPHA: ", alpha, "BETA: ", beta, "RprX: ", RprX, "RprY: ", RprY, "cpr: ", cpr, "c: ", c)
+
+	cPx, cPy := Curve.ScalarBaseMult(c)
+	log.Println("cP client: ", cPx, cPy)
+
+	blindSig := BlindServerSign(k, c, BytesToNum(privkey))
+	spr := BlindClientUnblind(alpha, blindSig)
+
+	if !SchnorrVerify(spr, RprX, RprY, Px, Py, "hello world") {
+		log.Fatal(errors.New("blind schnorr sigs don't match"))
+	} else {
+		log.Println("Blind Schnorr signatures work")
+	}
+
 }
