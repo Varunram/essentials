56d61cf45db09afbec39ed9cd674ce1a2fd2e513
Varunram <vrg2009@ymail.com>
2019-08-03 10:53:07 +0530

    add consts to emails package

---
+++ email/consts.go
@@ -0,0 +1,9 @@
+package email
+
+var From string
+var Pass string
+
+func SetConsts(from string, pass string) {
+	From = from
+	Pass = pass
+}
--- email/email.go
+++ email/email.go
@@ -2,33 +2,19 @@ package email
 
 import (
 	"github.com/pkg/errors"
-	"log"
 	"net/smtp"
-
-	"github.com/spf13/viper"
 )
 
 // SendMail is a handler for sending out an email to an entity, reading required params from the config file
 func SendMail(body string, to string) error {
 	var err error
-	// read from config.yaml in the working directory
-	viper.SetConfigType("yaml")
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err = viper.ReadInConfig()
-	if err != nil {
-		return errors.Wrap(err, "error while reading email values from config file")
-	}
-	log.Println("SENDING EMAIL: ", viper.Get("email"), viper.Get("password"))
-	from := viper.Get("email").(string)    // interface to string
-	pass := viper.Get("password").(string) // interface to string
-	auth := smtp.PlainAuth("", from, pass, "smtp.gmail.com")
+	auth := smtp.PlainAuth("", From, Pass, "smtp.gmail.com")
 	// to can also be an array of addresses if needed
-	msg := "From: " + from + "\n" +
+	msg := "From: " + From + "\n" +
 		"To: " + to + "\n" +
 		"Subject: OpenSolar Notification\n\n" + body
 
-	err = smtp.SendMail("smtp.gmail.com:587", auth, from, []string{to}, []byte(msg))
+	err = smtp.SendMail("smtp.gmail.com:587", auth, From, []string{to}, []byte(msg))
 	if err != nil {
 		return errors.Wrap(err, "smtp error")
 	}
