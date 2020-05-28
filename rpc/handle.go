6c3822af307021a39ee95e6a7474bf04bc33c9d5
Varunram <varunramganesh@gmail.com>
2020-05-28 19:30:21 +0530

    check for print msg

--- rpc/handle.go
+++ rpc/handle.go
@@ -8,12 +8,20 @@ import (
 // Err prints an error and returns to the caller
 func Err(w http.ResponseWriter, err error, status int, msgs ...string) bool {
 	var retmsg bool
+	var print bool
+
+	if len(msgs) > 0 {
+		print = true
+	}
+
 	if len(msgs) == 2 {
 		retmsg = true
 	}
 
 	if err != nil {
-		log.Println(err, msgs[0])
+		if print {
+			log.Println(err, msgs[0])
+		}
 		if retmsg {
 			ResponseHandler(w, StatusBadRequest, msgs[1])
 		} else {
